88a535c2b7e867403c05588148a610d81e2594e6
= <=>
2026-02-08 22:24:49 -0500

    adding types file for the data structures outlined

---
+++ src/types.ts
@@ -0,0 +1,25 @@
+export type IncidentType = 'accident' | 'theft' | 'fire' | 'water damage';
+
+export interface Claim {
+    policyId: string,
+    incidentType: IncidentType,
+    incidentDate: Date,
+    amountClaimed: number
+}
+
+export interface Policy {
+  policyId: string;
+  startDate: Date; // assuming UTC
+  endDate: Date; // assuming UTC
+  deductible: number;
+  coverageLimit: number;
+  coveredIncidents: IncidentType[];
+}
+
+export type ReasonCode = 'APPROVED' | 'POLICY_INACTIVE' | 'NOT_COVERED' | 'ZERO_PAYOUT';
+
+export interface ClaimEvaluationResult {
+    approved: boolean,
+    payout: number,
+    reason: ReasonCode
+}
