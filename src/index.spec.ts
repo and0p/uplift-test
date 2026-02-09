92270fa8ae7b4395432b6d2ca868060c3b093ed6
= <=>
2026-02-08 23:02:11 -0500

    Disabling verbatimModuleSyntax for simpler import/export, adding spec and simple function for claim policy time comparison.

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -1,5 +1,27 @@
-describe('jest', () => {
-    it('should work', () => {
-        expect(true).toBeTruthy();
+import type { Claim, Policy } from './types';
+
+import { doesClaimFallWithinPolicy } from './index';
+
+describe('Claim evaluation', () => {
+    describe('isPolicyCurrentlyActive', () => {
+        it('Should return true if the current time is after the policy start time and before the policy end time', () => {
+            const claim: Claim = {
+                policyId: 'test',
+                incidentType: 'accident',
+                incidentDate: new Date(2025, 6, 1),
+                amountClaimed: 50
+            }
+
+            const policy: Policy = {
+                policyId: 'test',
+                startDate: new Date(2025, 1, 1),
+                endDate: new Date(2025, 12, 1),
+                deductible: 10,
+                coverageLimit: 1000,
+                coveredIncidents: ['fire']
+            }
+
+            expect(doesClaimFallWithinPolicy(claim, policy)).toBeTruthy();
+        });
     })
 });
---
+++ src/index.ts
@@ -0,0 +1,5 @@
+import type { Claim, Policy } from './types';
+
+export function doesClaimFallWithinPolicy(claim: Claim, policy: Policy): boolean {
+    throw new Error('not implemented');
+}
